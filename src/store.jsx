b99b881481cf190f847a7bc6713de5ad79642de5
Dmitry Davydov <dmitry@MacBook-de-Dmitry.local>
2018-02-21 09:41:13 +0100

    merge material

--- src/store.jsx
+++ src/store.jsx
@@ -2,7 +2,7 @@ import { applyMiddleware, createStore } from 'redux';
 //import createLogger from 'redux-logger'
 //import thunk from 'redux-thunk'
 //import promise from 'redux-promise-middleware'
-import reducer from './reducers/index';
+import reducer from './reducers/searchOptionsReducer';
 
 const store = createStore(
   reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
